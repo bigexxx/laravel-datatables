17134b50eaa1d2d60797af4a4fb1db27691af8cd
Ramil Rafikov <bigexxx@gmail.com>
2024-03-28 17:45:57 +0500

    Cast request inputs to integer in QueryDataTable
    The changes update the limit and start parameters in the paging method of QueryDataTable. The parameters now cast user inputs as integers. This adjustment works as a safeguard against potential data type inconsistencies.

--- src/QueryDataTable.php
+++ src/QueryDataTable.php
@@ -620,12 +620,12 @@ class QueryDataTable extends DataTableAbstract
      */
     public function paging()
     {
-        $limit = (int) $this->request->input('length') > 0 ? $this->request->input('length') : 10;
+        $limit = (int) $this->request->input('length') > 0 ? (int) $this->request->input('length') : 10;
         if (is_callable($this->limitCallback)) {
             $this->query->limit($limit);
             call_user_func_array($this->limitCallback, [$this->query]);
         } else {
-            $this->query->skip($this->request->input('start'))->take($limit);
+            $this->query->skip((int) $this->request->input('start'))->take($limit);
         }
     }
 
